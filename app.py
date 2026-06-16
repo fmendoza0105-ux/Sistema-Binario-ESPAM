66cb6a5cc1d5e77e5eb2c5a064d5d01a6e91fa50
fmendoza0105-ux <fmendoza0105@utm.edu.ec>
2026-06-15 23:54:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -66,9 +66,7 @@ st.markdown(f"""
 <h1>SISTEMA BINARIO</h1>
 <h3>Escuela Superior Politécnica Agropecuaria de Manabí</h3>
 <h3>Manuel Félix López - Feria de Ciencias 2026</h3>
-<h4>👩‍🏫 Expositora Oficial
-Mgtr. Virginia Lucciola Mendoza Zambrano
-Docente y Magíster </h4>
+<h4>👩‍🏫 Expositora Oficial Mgtr. Virginia Lucciola Mendoza Zambrano - Docente </h4>
 </div>
 <div></div>
 </div>
